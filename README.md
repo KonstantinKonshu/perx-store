02ef8bf42a1c212c2fffec8918fa293232cc1b09
KonshuKonstantin <kostyanfly@gmail.com>
2024-03-15 00:32:13 +0600

    initial commit: настройка webpack, eslint, prettier, tsconfig

---
+++ README.md
@@ -0,0 +1,39 @@
+# PERX-store
+
+## Stack
+
+-   React
+-   Redux (Redux Toolkit, RTK Query)
+-   TypeScript
+-   Webpack
+-   Ant Design
+
+## Ссылки
+
+-   [Тестовое задание](https://git.perx.ru/-/snippets/15)
+
+## Архитектура
+
+На этом проекте используется [feature-slice-design](https://feature-sliced.design/ru/docs/get-started/overview#basics)
+
+## Getting started
+
+### Клонируем
+
+`git clone git@gitlab.com:KonstantinKonshu/perx-store.git`
+
+### Копируем .env
+
+1. `cd perx-store`
+2. `copy .env.example .env`
+3. `yarn`
+
+## Запуск
+
+### Development:
+
+`yarn dev`
+
+### Production:
+
+...скоро
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path  = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require("dotenv-webpack")
+
+require('dotenv').config()
+
+module.exports = (_env, argv) => {
+  const isProduction = argv.mode === "production";
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.[chunkhash:8].js",
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
+        },
+        {
+          test: /\.(s[ac]|c)ss$/,
+          exclude: /node_modules/,
+          use: [ isProduction ? MiniCssExtractPlugin.loader : "style-loader", "css-loader", "sass-loader"]
+        },
+        {
+          test: /\.(gif|jpe?g|tiff|png|webp|bmp|svg|eot|ttf|woff|woff2)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, './public/index.html')
+    }),
+      new Dotenv()
+    ],
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      alias: {
+        '@public': path.resolve(__dirname, './public'),
+        '@app': path.resolve(__dirname, "./src/app"),
+        '@pages': path.resolve(__dirname, "./src/pages"),
+        '@shared': path.resolve(__dirname, "./src/shared"),
+        '@entities': path.resolve(__dirname, "./src/entities"),
+        '@widgets': path.resolve(__dirname, "./src/widgets"),
+        '@features': path.resolve(__dirname, "./src/features")
+      }
+    },
+    devtool: "source-map",
+    devServer: {
+      hot: true,
+      port: process.env.PORT
+    },
+}};
